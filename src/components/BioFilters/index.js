63ae0488f92d7918b9db3ec188fc62e15c6c9fd3
Nick Cummings <nickmcummings@gmail.com>
2019-03-17 18:17:54 -0700

    Stubbed out this notion of 'bio-filters'. They're not for filtering out bacteria, or anything like that. It's just a cool idea I had for sorting through all the different stuff I do.
    Basically, here's how it works:
    
    1) Hovering over a filter will highlight everything it pertains to on the page and fade out everything it does not pertain to
    2) Clicking a filter will apply it (maybe -- possibly hovering will be enough of an effect)

---
+++ src/components/BioFilters/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './style.css'
+
+const filters = [
+    {
+        name: 'writer',
+        displayName: 'writer',
+    },
+    {
+        name: 'game-designer',
+        displayName: 'game designer',
+    },
+    {
+        name: 'web-developer',
+        displayName: 'web developer',
+    },
+    {
+        name: 'podcaster',
+        displayName: 'podcaster',
+    },
+    {
+        name: 'human-being',
+        displayName: 'human being',
+    },
+]
+
+class FilterButton extends Component {
+    static propTypes = {
+        name: PropTypes.string,
+        displayName: PropTypes.string,
+    }
+    
+    render() {
+        return (
+            <div>
+                <button className="filter" type="button" name={this.props.name}>
+                    {this.props.displayName}
+                </button>
+            </div>
+        )
+    }
+}
+
+class BioFilters extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <p>I'm a</p>
+        {filters.map(filter => <FilterButton key={filter.name} {...filter}/>)}
+        <p>living in Portland, Oregon.</p>
+      </div>
+    )
+  }
+}
+
+export default BioFilters
